2177dc8eaceb7f078000845ef668620894e4b958
IasmineSilva <iasmine948@gmail.com>
2022-09-04 17:28:48 -0300

    praticandoc#

---
+++ Primeiro/Course/Course/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            int N1 = 3 + 4 * 2;
+            int N2 = (3 + 4) * 2;
+            int N3 = 17 % 3;
+            double N4 = 10.0 / 8.0;
+            double a = 1.0, b = -3.0, c = -4.0;
+            double delta = b * b - 4.0 * a * c;
+            double y1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            Console.WriteLine(N1);
+            Console.WriteLine(N2);
+            Console.WriteLine(N3);
+            Console.WriteLine(N4);
+            Console.WriteLine(delta);
+            Console.WriteLine(y1);
+
+        }
+    }
+}
+
