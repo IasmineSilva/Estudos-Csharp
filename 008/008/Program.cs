d590e5f6dd916a749d5bf5f4d9a8e7f812c2615c
IasmineSilva <iasmine948@gmail.com>
2023-01-07 14:05:18 -0300

    att

---
+++ 008/008/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008
+{
+    internal class Program
+    {
+private string _nome;
+        private double _preco;
+        private int _quantidade;
+        public Produto()
+        {
+        }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+        public string GetNome()
+        {
+            return _nome;
+        }
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }
+        public double GetPreco()
+        {
+            return _preco;
+        }
+    }
+    }
+
